6c22b5e7f6cbbff74767802f374b31b4aa7cf2ff
Roman Prytulenko <roman.prytulenko@gmail.com>
2021-04-25 14:02:44 +0300

    Added logger service. Changed controller for testing. Removed useless code.

---
+++ Pro.Universal/Pro.Universal.WebAPI/Controllers/TestController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Pro.Universal.Common.Services.Interfaces;
+
+namespace Pro.Universal.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILoggerManager _logger;
+
+        public TestController(ILoggerManager logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogInfo("Here is info message from the controller.");
+            _logger.LogDebug("Here is debug message from the controller.");
+            _logger.LogWarn("Here is warn message from the controller.");
+            _logger.LogError("Here is error message from the controller.");
+
+            return Summaries;
+        }
+    }
+}
--- Pro.Universal/Pro.Universal.WebAPI/Startup.cs
+++ Pro.Universal/Pro.Universal.WebAPI/Startup.cs
@@ -1,17 +1,15 @@
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.AzureAD.UI;
+using System;
+using System.IO;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
+using NLog;
+
+using Pro.Universal.WebAPI.Extensions;
 
 namespace Pro.Universal.WebAPI
 {
@@ -19,6 +17,7 @@ namespace Pro.Universal.WebAPI
     {
         public Startup(IConfiguration configuration)
         {
+            LogManager.LoadConfiguration(String.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
             Configuration = configuration;
         }
 
@@ -27,8 +26,7 @@ namespace Pro.Universal.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAuthentication(AzureADDefaults.BearerAuthenticationScheme)
-                .AddAzureADBearer(options => Configuration.Bind("AzureAd", options));
+            services.ConfigureLoggerService();
             services.AddControllers();
         }
 
